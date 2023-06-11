6bed848096d2413198025a05bf82f440b730b101
Joanna Gajewska <joanna.gajewska95@gmail.com>
2023-06-11 21:31:50 +0200

    2nd change

--- Lista.py
+++ Lista.py
@@ -2,3 +2,17 @@ Lista_zakupów = {'Piekarnia': ['chleb', "bułki", "pączek"],
              'Warzywniak': ["marchew", "seler", "rukola"],}
 for x, y in Lista_zakupów.items():
   print(f"Idę do {x}, kupuję tu następujące rzeczy: {y}".title())
+
+  d = {
+    "warzywniak": ["marchew", "seler", "rukola"],
+    "piekarnia": ["chleb", "bułki", "pączek"]
+  }
+values = list(d.values())
+
+count = 0
+# flat_list = []
+for sublist in values:
+    count += len(sublist)
+    # flat_list += sublist
+print(f"w sumie kupuję {count} produktów")
+
