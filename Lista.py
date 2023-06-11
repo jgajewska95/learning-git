105f74e5593c9a75db729e155076dacb561f6df4
Joanna Gajewska <joanna.gajewska95@gmail.com>
2023-06-11 21:29:27 +0200

    1st change

--- Lista.py
+++ Lista.py
@@ -1,2 +1,4 @@
 Lista_zakupów = {'Piekarnia': ['chleb', "bułki", "pączek"],
              'Warzywniak': ["marchew", "seler", "rukola"],}
+for x, y in Lista_zakupów.items():
+  print(f"Idę do {x}, kupuję tu następujące rzeczy: {y}".title())
